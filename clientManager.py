b4618fcf30379c13dea68de78657528f800410b0
Ritvik-rc <77318928+Ritvik-rc@users.noreply.github.com>
2021-12-10 13:13:02 +0530

    Update clientManager.py

--- clientManager.py
+++ clientManager.py
@@ -3,6 +3,6 @@ import socketHandler
 
 client = socketHandler.socketHandler()
 session = sessionManager.sessionManager()
-clientSocket = client.createConnection("172.17.0.2",3030)
+clientSocket = client.createConnection(("172.17.0.2",3030))
 message = recieveDataFromSession(clientSocket)
 print(message)
